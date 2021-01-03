7aa2e21d5325ca07923a32a5a9e7fe10a08e4021
chrisberanek <74572231+chrisberanek@users.noreply.github.com>
2021-01-03 17:09:22 -0600

    Game Class

--- src/js/script.js
+++ src/js/script.js
@@ -50,4 +50,64 @@ class Card {
     } 
 }  
   
-  
+class Game {  
+
+    constructor() { 
+
+        this.round = 0 
+
+        this.warPile = [] 
+
+        this.players = [new Player ("Player One"), new Player ("Player Two")] 
+
+        this.deck = new Deck () 
+
+        this.deck.shuffle(); 
+
+    }  
+
+    deal() {     
+
+        for (let i = 0; i < this.deck.length; i++) { 
+
+            if (i % 2 === 0) { 
+
+                this.players[0].hand.unshift(this.deck.cards[i]) 
+
+            } else { 
+
+                this.players[1].hand.unshift(this.deck.cards[i]) 
+
+            } 
+
+        } 
+
+        this.deck = [] 
+
+        this.startRound() 
+
+    } 
+
+    startRound() { 
+
+        this.round += 1; 
+
+        this.flipCards(); 
+
+    } 
+
+    flipCards() { 
+
+        this.players[0].cardsInPlay.unshift(this.players[0].hand[0]); 
+
+        this.players[0].hand.shift(); 
+
+        this.players[1].cardsInPlay.unshift(this.players[1].hand[0]); 
+
+        this.players[1].hand.shift(); 
+
+        console.log(`${this.players[0].name} flipped ${this.players[0].cardsInPlay[0].rank} of ${this.players[0].cardsInPlay[0].suit}\n\n${this.players[1].name} flipped ${this.players[1].cardsInPlay[0].rank} of ${this.players[1].cardsInPlay[0].suit}`) 
+
+        this.compareRank(); 
+
+    }   
